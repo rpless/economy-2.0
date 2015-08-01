ea0d4f4251907eae51fef9912c0ed46e69d14d81
Spencer Florence <florence@ccs.neu.edu>
2015-08-01 13:52:58 -0500

    abstracting tests

--- server/src/test/scala/io/github/rpless/OcrSpec.scala
+++ server/src/test/scala/io/github/rpless/OcrSpec.scala
@@ -11,18 +11,25 @@ class OcrSpec extends FlatSpec with Matchers {
   val ocr = Ocr().get
 
   "Ocr" should "get empty text from empty image" in {
-    val img = new BufferedImage(1024, 768, BufferedImage.TYPE_INT_ARGB)
-    ocr.getRawText(img) shouldBe ""
+    testWithText("")
   }
 
   "Ocr" should "get simple string from generated image" in {
-    val text = "LALALALALALAE FA#R #WR W# ))W$T:W:# RW :FW :#!{}|"
+    testWithText("LALALALALALAE FA#R #WR W# ))W$T:W:# RW :FW :#!{}|")
+  }
+
+  def testWithText(text: String): Unit = {
+    val img = imageWithText(text)
+    ocr.getRawText(img) shouldBe text
+  }
+
+  def imageWithText(text: String): BufferedImage = {
     val img = new BufferedImage(1024, 768, BufferedImage.TYPE_INT_ARGB)
     val graphics = img.getGraphics
     graphics.fillRect(0, 0, 200, 50);
     graphics.setColor(Color.BLACK);
     graphics.setFont(new Font("Arial Black", Font.BOLD, 20));
     graphics.drawString(text,10,50)
-    ocr.getRawText(img).get shouldBe text
+    return img
   }
 }
