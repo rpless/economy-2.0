038f20591e6585433249a6e7993e027475aafb46
Ryan Plessner <plessner.ryan@gmail.com>
2015-08-01 14:52:20 -0400

    Add tessdata and attempt to get it to be loaded

--- server/src/main/scala/io/github/rpless/Ocr.scala
+++ server/src/main/scala/io/github/rpless/Ocr.scala
@@ -1,24 +1,39 @@
 package io.github.rpless
 
 import java.awt.image.BufferedImage
-
-import net.sourceforge.tess4j.Tesseract
-
-import scala.util.Try
+import net.sourceforge.tess4j.TessAPI1._
+import net.sourceforge.tess4j.util.ImageIOHelper
+import scala.util.{Success, Failure, Try}
 
 /**
  */
 class Ocr {
-  val api = new Tesseract()
+  val api = TessBaseAPICreate()
+//  val dataPath = getClass.getResource("/").getPath
+  private val success = TessBaseAPIInit3(api, "./", "eng") == 0
 
-  def getRawText(img: BufferedImage): Try[String] = {
-    Try(api.doOCR(img))
+  def getRawText(img: BufferedImage): String = {
+    val pixelSize = img.getColorModel.getPixelSize
+    val bpp = pixelSize / 8.0
+    val bpl = Math.ceil(img.getWidth * bpp)
+    val buf = ImageIOHelper.convertImageData(img)
+
+    TessBaseAPISetImage(api, buf, img.getWidth, img.getHeight, bpp.toInt, bpl.toInt)
+
+    val ptr = TessBaseAPIGetUTF8Text(api)
+    val str = ptr.getString(0)
+    TessDeleteText(ptr)
+
+    str
   }
 }
 
 object Ocr {
+
   System.setProperty("jna.encoding", "UTF8")
   def apply(): Try[Ocr] = {
-    Try(new Ocr)
+    val ocr = new Ocr
+    if (ocr.success) Success(ocr)
+    else Failure(new RuntimeException("OCR init failed"))
   }
 }
--- server/src/test/scala/io/github/rpless/OcrSpec.scala
+++ server/src/test/scala/io/github/rpless/OcrSpec.scala
@@ -10,6 +10,6 @@ class OcrSpec extends FlatSpec with Matchers {
   "Ocr" should "get text from generated image" in {
     val ocr = Ocr().get
     val img = new BufferedImage(1024, 768, BufferedImage.TYPE_INT_ARGB)
-    ocr.getRawText(img).get shouldBe ""
+    ocr.getRawText(img) shouldBe ""
   }
 }
