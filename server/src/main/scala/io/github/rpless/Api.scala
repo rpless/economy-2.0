4b58663e9ee5cb341993bab1e7eabfa0b9a9d130
Spencer Florence <florence@ccs.neu.edu>
2015-08-01 20:29:03 -0500

    placeholder authorizer

--- server/src/main/scala/io/github/rpless/Api.scala
+++ server/src/main/scala/io/github/rpless/Api.scala
@@ -9,12 +9,13 @@ import io.finch._
 object Api {
   val ocr = Ocr().get
 
-  //val authorizer = RequestReader.value[Boolean](true)
+  val authorizer = RequestReader.value[Boolean](true).should(ValidationRule[Boolean]("auth"){f => f})
+
+  val fileToImage = fileUpload("image").embedFlatMap({file =>
+    val img = ImageIO.read(file.getFile)
+    TextInterpreter(ocr.getRawText(img)).toFuture
+  })
 
   def receiptEndpoint: Router[RequestReader[Seq[TextInterpreter.Item]]] =
-    Post / "receipt" /> fileUpload("image").embedFlatMap({file =>
-      val img = ImageIO.read(file.getFile)
-      TextInterpreter(ocr.getRawText(img)).toFuture
-    })
-
+    Post / "receipt" /> (authorizer :: fileToImage).asTuple.embedFlatMap(_._2.toFuture)
 }
